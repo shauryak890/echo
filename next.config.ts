a92a2512c19a21fbc943d93cc14c145c31f73bf9
Aimans30 <aiman.singh30@gmail.com>
2025-12-17 22:20:02 +0530

    Fix Next.js 16 build by forcing webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack(config) {
+    config.watchOptions = {
+      ignored: ['**/blog-for-echopulse/**']
+    }
+    return config
+  }
+}
 
-export default nextConfig;
+export default nextConfig
